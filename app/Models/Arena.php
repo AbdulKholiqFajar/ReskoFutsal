21d8cef71a1b662b55c3559e85b760382c0934e5
AbdulKholiqFajar <abdulkholiqfajar10>
2024-07-16 14:01:11 +0700

    PROJECT UAS PEMOGRAMAN WEB 2 LARAVEL

---
+++ app/Models/Arena.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\Cast\Bool_;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Arena extends Model implements HasMedia
+{
+    use HasFactory,InteractsWithMedia;
+
+    protected $appends = [
+        'photo',
+        'photo_url',
+    ];
+    
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function getPhotoAttribute()
+    {
+        $file = $this->getMedia('photo')->last();
+
+        if ($file) {
+            $file->url       = $file->getUrl();
+        }
+
+        return $file;
+    }
+    public function getPhotoUrlAttribute()
+    {
+        $file = $this->getMedia('photo')->last();
+        // dd($file);
+        return $file ? asset('storage/'.$file->id.'/'.$file->file_name) : '';
+    }
+
+    public function getStatusAttribute($input){
+        return [
+            0 => 'In Active',
+            1 => 'Active'
+        ][$input];
+    }
+
+    public function booking(){
+        return $this->hasOne(Booking::class, 'arena_id');
+    }
+}
